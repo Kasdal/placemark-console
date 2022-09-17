fd2d9a58b1b64a553f96dd3f25480274ca370739
Milan Ples <plesmilan@hotmail.com>
2022-09-17 17:59:12 +0100

    Initial Commit

---
+++ src/main/kotlin/org/setu/placemark/main.kt
@@ -0,0 +1,9 @@
+package org.setu.placemark
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+fun main(args: Array<String>){
+    println("Placemark Kotlin App Version 1.0")
+    logger.info { "Launching Placemark Console App" }
+}
