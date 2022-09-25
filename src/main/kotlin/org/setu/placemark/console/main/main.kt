2cd6647e3aad5bee9e92012513ddad7a8c1f71f3
Milan Ples <plesmilan@hotmail.com>
2022-09-25 14:00:23 +0100

    Lab 2 Finish

--- src/main/kotlin/org/setu/placemark/console/main/main.kt
+++ src/main/kotlin/org/setu/placemark/console/main/main.kt
@@ -1,11 +1,11 @@
 package org.setu.placemark.console.main
 
 import mu.KotlinLogging
+import org.setu.placemark.console.models.PlacemarkModel
 
 private val logger = KotlinLogging.logger {}
 
-var title = ""
-var description = ""
+val placemarks = ArrayList<PlacemarkModel>()
 
 fun main(args: Array<String>) {
     logger.info { "Launching Placemark Console App" }
@@ -19,6 +19,8 @@ fun main(args: Array<String>) {
             1 -> addPlacemark()
             2 -> updatePlacemark()
             3 -> listPlacemarks()
+            4 -> searchPlacemark()
+            5 -> dummyData()
             -1 -> println("Exiting App")
             else -> println("Invalid Option")
         }
@@ -36,6 +38,8 @@ fun menu() : Int {
     println(" 1. Add Placemark")
     println(" 2. Update Placemark")
     println(" 3. List All Placemarks")
+    println(" 4. Search Placemarks")
+    println(" 5.Load Dummy Data")
     println("-1. Exit")
     println()
     print("Enter Option : ")
@@ -48,26 +52,81 @@ fun menu() : Int {
 }
 
 fun addPlacemark(){
-
+    var aPlacemark = PlacemarkModel()
     println("Add Placemark")
     println()
     print("Enter a Title : ")
-    title = readLine()!!
+    aPlacemark.title = readLine()!!
     print("Enter a Description : ")
-    description = readLine()!!
-    println("You entered [ $title ] for title and [ $description ] for description")
+    aPlacemark.description = readLine()!!
+
+    if (aPlacemark.title.isNotEmpty() && aPlacemark.description.isNotEmpty()) {
+        aPlacemark.id = placemarks.size.toLong()
+        placemarks.add(aPlacemark.copy())
+        logger.info("Placemark Added : [ $aPlacemark ]")
+    }
+    else
+        logger.info("Placemark Not Added")
 }
 
 fun updatePlacemark() {
     println("Update Placemark")
     println()
-    print("Enter a new Title for [ $title ] : ")
-    title = readLine()!!
-    print("Enter a new Description for [ $description ] : ")
-    description = readLine()!!
-    println("You updated [ $title ] for title and [ $description ] for description")
+    listPlacemarks()
+    var searchId = getId()
+    val aPlacemark = search(searchId)
+
+    if(aPlacemark != null) {
+        print("Enter a new Title for [ " + aPlacemark.title + " ] : ")
+        aPlacemark.title = readLine()!!
+        print("Enter a new Description for [ " + aPlacemark.description + " ] : ")
+        aPlacemark.description = readLine()!!
+        println(
+            "You updated [ " + aPlacemark.title + " ] for title " +
+                    "and [ " + aPlacemark.description + " ] for description"
+        )
+    }
+    else
+        println("Placemark Not Updated...")
 }
 
 fun listPlacemarks() {
-    println("You Chose List All Placemarks")
+    println("List All Placemarks")
+    println()
+    placemarks.forEach { logger.info("${it}") }
+    println()
+}
+
+fun searchPlacemark() {
+
+    var searchId = getId()
+    val aPlacemark = search(searchId)
+
+    if(aPlacemark != null)
+        println("Placemark Details [ $aPlacemark ]")
+    else
+        println("Placemark Not Found...")
+}
+
+fun getId() : Long {
+    var strId : String? // String to hold user input
+    var searchId : Long // Long to hold converted id
+    print("Enter id to Search/Update : ")
+    strId = readLine()!!
+    searchId = if (strId.toLongOrNull() != null && !strId.isEmpty())
+        strId.toLong()
+    else
+        -9
+    return searchId
+}
+
+fun search(id: Long) : PlacemarkModel? {
+    var foundPlacemark: PlacemarkModel? = placemarks.find { p -> p.id == id }
+    return foundPlacemark
+}
+
+fun dummyData() {
+    placemarks.add(PlacemarkModel(1, "New York New York", "So Good They Named It Twice"))
+    placemarks.add(PlacemarkModel(2, "Ring of Kerry", "Some place in the Kingdom"))
+    placemarks.add(PlacemarkModel(3, "Waterford City", "You get great Blaas Here!!"))
 }
